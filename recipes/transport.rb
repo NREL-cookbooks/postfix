d8ec5a3208c4fd42671fdd2d7846fc91cbd3e475
Nick Muerdter <nick.muerdter@nrel.gov>
2013-01-17 13:30:18 -0700

    Move custom transport config to separate recipe.

---
+++ recipes/transport.rb
@@ -0,0 +1,13 @@
+execute "rebuild-transport" do
+  command "postmap /etc/postfix/transport"
+  action :nothing
+  notifies :restart, "service[postfix]"
+end
+
+template "/etc/postfix/transport" do
+  source "transport.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  notifies :run, "execute[rebuild-transport]"
+end
